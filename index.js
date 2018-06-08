4ca4c610f3ace87dcc75391f51fa58b50dbe8dff
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-06-08 05:10:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // write your code below!
 function happyHolidays() {return "Happy holidays!"
-function happyHolidaysTo(recipient) {return 
+function happyHolidaysTo(recipient) {return 'Happy holidays, ${recipient}!'}
+fuction happyHolidayTo()
